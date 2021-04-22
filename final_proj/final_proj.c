898c83ced107ff52cbea887cd2fc5c8c6783a011
Jun Wei <lowjunwei.ljw@gmail.com>
2021-04-22 14:07:16 +0800

    added nodes in proximity and threshold logic

--- final_proj/final_proj.c
+++ final_proj/final_proj.c
@@ -26,7 +26,8 @@ static struct pt pt;
 static data_packet_struct received_packet;
 static data_packet_struct data_packet;
 int nodes[50][3];
-int numNodes = 0;
+int num_nodes = 0;
+int nodes_in_proximity = 0;
 unsigned long curr_timestamp;
 /*---------------------------------------------------------------------------*/
 PROCESS(cc2650_nbr_discovery_process, "cc2650 neighbour discovery process");
@@ -35,9 +36,7 @@ AUTOSTART_PROCESSES(&cc2650_nbr_discovery_process);
 int indexOf( const int a[][3], int size, int value )
 {
     int index = 0;
-
     while ( index < size && a[index][0] != value ) ++index;                                               
-
     return ( index == size ? -1 : index );
 }
 
@@ -47,31 +46,47 @@ broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
   leds_on(LEDS_GREEN);
   memcpy(&received_packet, packetbuf_dataptr(), sizeof(data_packet_struct));
 
-  int currNode = (int)received_packet.src_id;
-  int node_index = indexOf(nodes, numNodes+1, currNode);
+  int threshold = 0;
+  int curr_node = (int)received_packet.src_id;
+  int node_index = indexOf(nodes, num_nodes+1, curr_node);
+  int curr_rssi = (signed short)packetbuf_attr(PACKETBUF_ATTR_RSSI);
   curr_timestamp = clock_time() / CLOCK_SECOND;
-  // Detection of a new node
-  if (node_index == -1) {
-    printf("%lu DETECT %d\n", curr_timestamp, currNode);
-    numNodes++;
-    // Add new nodeID to array
-    nodes[numNodes][0] = currNode;
-    // Add to both current and most recent timestamps
-    nodes[numNodes][1] = nodes[numNodes][2] = curr_timestamp;
+
+  if (nodes_in_proximity > 10) {
+    threshold = -100;
+  } else if (nodes_in_proximity > 5) {
+    threshold = -75;
+  } else {
+    threshold = -50;
   }
-  else {
-    // Detection of nodes that left but came back
-    if (nodes[node_index][1] == 0) {
-      printf("%lu DETECT %d\n", curr_timestamp, currNode);
-      nodes[node_index][1] = nodes[node_index][2] = (int)curr_timestamp;
+
+  if (curr_rssi > threshold)
+    // Detection of a new node
+    if (node_index == -1) {
+      printf("%lu DETECT %d\n", curr_timestamp, curr_node);
+      nodes_in_proximity++;
+      pritnf("New node: %d || Nodes in proximity: %d", curr_node, nodes_in_proximity);
+      num_nodes++;
+      // Add new nodeID to array
+      nodes[num_nodes][0] = curr_node;
+      // Add to both current and most recent timestamps
+      nodes[num_nodes][1] = nodes[num_nodes][2] = curr_timestamp;
     }
-    // For nodes already in the array
     else {
-      printf("Received Node %d || RSSI: %d\n", currNode, (signed short)packetbuf_attr(PACKETBUF_ATTR_RSSI));
-      // Update only the most recent timestamp
-      nodes[node_index][2] = (int)curr_timestamp;
+      // Detection of nodes that left but came back
+      if (nodes[node_index][1] == 0) {
+        printf("%lu DETECT %d\n", curr_timestamp, curr_node);
+        nodes_in_proximity++;
+        printf("Node: %d has returned || Nodes in proximity: %d", curr_node, nodes_in_proximity);
+        nodes[node_index][1] = nodes[node_index][2] = (int)curr_timestamp;
+      }
+      // For nodes already in the array
+      else {
+        printf("Received Node %d || RSSI: %d\n", curr_node, curr_rssi);
+        // Update only the most recent timestamp
+        nodes[node_index][2] = (int)curr_timestamp;
+      }
     }
-  }
 
   leds_off(LEDS_GREEN);
 }
@@ -129,9 +144,9 @@ char sender_scheduler(struct rtimer *t, void *ptr) {
     curr_timestamp = clock_time() / CLOCK_SECOND;
     //printf("TIMESTAMP (MS): %lu\n", curr_timestamp);
     //printf("TIMESTAMP (SEC): %lu\n", curr_timestamp/CLOCK_SECOND);
-    
+
     for(i=0; i<50; i++) {
-      if ((((int)curr_timestamp - nodes[i][2]) > 30000) && (nodes[i][2] > 0)) {
+      if ((((int)curr_timestamp - nodes[i][2]) > 30) && (nodes[i][2] > 0)) {
           // nodes[i][1] = first detected time
           // nodes[i][2] = most recent time
           printf("%lu LEAVE %d\n", curr_timestamp, nodes[i][0]);
@@ -140,6 +155,8 @@ char sender_scheduler(struct rtimer *t, void *ptr) {
           // Reset nodes that left
           nodes[i][1] = nodes[i][2] = 0;
           printf("Time in proximity: %d\n", time_in_proximity);
+          nodes_in_proximity--;
+          printf("Nodes in proximity: %d", nodes_in_proximity);
       }
       else {
         continue;
